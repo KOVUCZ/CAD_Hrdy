659b39f7c408f122a1bc41f9ce08bb883e6ba113
hrdy.adam <hrdy.adam@D20-17>
2023-04-24 13:27:45 +0200

    Přidejte soubory projektu.

---
+++ CAD_Hrdy/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CAD_Hrdy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {   
+        private List<point> points = new List<point>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ActiveCanvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed)
+            {
+                var location = e.GetPosition(ActiveCanvas);
+                point point = new point(location.X, location.Y);
+                points.Add(point);
+                point.AddToCanvas(ActiveCanvas);
+            }
+        }
+    }
+}
---
+++ CAD_Hrdy/point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace CAD_Hrdy
+{
+    public class point
+    {
+        private object ellipse;
+
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void AddToCanvas(Canvas canvas)
+        {
+            Ellipse ellipse = new Ellipse();
+            ellipse.Fill = Brushes.Black;
+            ellipse.Width = 8;
+            ellipse.Height = 8;
+            Canvas.SetLeft(ellipse, X - 4);
+            Canvas.SetTop(ellipse, Y - 4);
+            canvas.Children.Add(ellipse);
+        }
+    }
+}
